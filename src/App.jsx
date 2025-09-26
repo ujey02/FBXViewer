760d8fc0af23aa797b42f2fd5ac7e47b10817a34
jungeun <je.yoo@kaistudio.co.kr>
2025-09-26 12:32:17 +0900

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+// App.jsx
+import React from 'react';
+import FBXViewer from './FBXViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <FBXViewer />
+    </div>
+  );
+}
+
+export default App;
